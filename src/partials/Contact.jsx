3b583a97f2abf6ec6513d2281aa4ff12bd73d6b0
Eneilac <eduarneila@gmail.com>
2024-07-11 10:54:35 +0200

    update

--- src/partials/Contact.jsx
+++ src/partials/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
     const handleChange = (e) => {
         setFormData({ ...formData, [e.target.name]: e.target.value });
     };
-    
+
     return (
         <section id="contact" className="relative">
             <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
@@ -55,13 +55,9 @@ const Contact = () => {
                         </div>
                     </div>
                 </div>
-                <form
-                    name="contact"
-                    method="POST"
-                    className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
-                >
+                <form name="contact" method="POST" className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0" >
+                 
                     <input type="hidden" name="form-name" value="contact" />
-
                     <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
                         ¡Contáctame!
                     </h2>
